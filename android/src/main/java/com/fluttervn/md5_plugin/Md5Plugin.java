d1ce16e29f9474380e3833bb06baac7aa2362245
Lap Ming Lee <lapapply@protonmail.ch>
2021-10-20 01:21:57 +0800

    V2 embedding

--- android/src/main/java/com/fluttervn/md5_plugin/Md5Plugin.java
+++ android/src/main/java/com/fluttervn/md5_plugin/Md5Plugin.java
@@ -1,5 +1,6 @@
 package com.fluttervn.md5_plugin;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -13,79 +14,79 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
 import android.util.Base64;
 import android.util.Log;
 
-/** Md5Plugin */
-public class Md5Plugin implements MethodCallHandler {
-  /** Plugin registration. *//*
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "md5_plugin");
-    channel.setMethodCallHandler(new Md5Plugin());
-  }
+/**
+ * Md5Plugin
+ */
+public class Md5Plugin implements MethodCallHandler, FlutterPlugin {
+    private final int BUFFER_SIZE = 1024 * 8;
 
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
-  }*/
-
-  private final int BUFFER_SIZE = 1024 * 8;
-
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "md5_plugin");
-    channel.setMethodCallHandler(new Md5Plugin());
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if("getMD5".equals(call.method)){
-      String path= call.argument("file_path");
-      Log.d("MD5Plugin","getMD5 @filepath="+path);
-      result.success(getFileChecksum(path));
-    } else {
-      result.notImplemented();
-    }
-  }
-
-  private String getFileChecksum(String filePath) {
-    File file=new File(filePath);
-    if (file.exists()) {
-      try {
-        InputStream is = new FileInputStream(file);
-        return getStreamChecksum(is);
-      } catch (FileNotFoundException e) {
-      }
-    }
-    return null;
-  }
-
-  private String getStreamChecksum(InputStream is) {
-    try {
-      MessageDigest md = MessageDigest.getInstance("MD5");
-      byte[] buffer = new byte[BUFFER_SIZE];
-      int numRead;
-      while ((numRead = is.read(buffer)) != -1) {
-        md.update(buffer, 0, numRead);
-      }
-      byte[] digest = md.digest();
-      String checksum = new String(Base64.encode(digest, Base64.DEFAULT));
-      String result= checksum.trim();
-      Log.d("MD5Plugin","getStreamChecksum @result="+result);
-      return result;
-    } catch (NoSuchAlgorithmException e) {
-    } catch (IOException e) {
-    } finally {
-      try {
-        is.close();
-      } catch (IOException e) {
-      }
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "md5_plugin");
+        channel.setMethodCallHandler(new Md5Plugin());
     }
 
-    return null;
-  }
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        final MethodChannel channel = new MethodChannel(binding.getBinaryMessenger(), "md5_plugin");
+        channel.setMethodCallHandler(new Md5Plugin());
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if ("getMD5".equals(call.method)) {
+            String path = call.argument("file_path");
+            Log.d("MD5Plugin", "getMD5 @filepath=" + path);
+            result.success(getFileChecksum(path));
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    private String getFileChecksum(String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            try {
+                InputStream is = new FileInputStream(file);
+                return getStreamChecksum(is);
+            } catch (FileNotFoundException e) {
+            }
+        }
+        return null;
+    }
+
+    private String getStreamChecksum(InputStream is) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int numRead;
+            while ((numRead = is.read(buffer)) != -1) {
+                md.update(buffer, 0, numRead);
+            }
+            byte[] digest = md.digest();
+            String checksum = new String(Base64.encode(digest, Base64.DEFAULT));
+            String result = checksum.trim();
+            Log.d("MD5Plugin", "getStreamChecksum @result=" + result);
+            return result;
+        } catch (NoSuchAlgorithmException e) {
+        } catch (IOException e) {
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+            }
+        }
+
+        return null;
+    }
 }
